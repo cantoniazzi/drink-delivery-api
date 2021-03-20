05debd210f999a37658368353db2ae2cc0cb7ec9
Cássio Antoniazzi <cassiosvaldo@gmail.com>
2021-03-20 15:29:53 -0300

    (feature) - adds logger client (#4)
    * adds log config
    - adds log formatter;
    - adds sentry client;
    
    * changed pp port to fix socket conflict with sentry agent.

---
+++ app/common/logger.py
@@ -0,0 +1,54 @@
+from datetime import datetime
+from json import dumps
+from logging import Formatter
+from logging import getLogger
+from logging import StreamHandler
+from os import getenv
+from socket import gethostname
+from sys import stdout
+from traceback import format_exc
+
+from dynaconf import settings
+import sentry_sdk
+
+
+def create_logger():
+    logger = getLogger()
+    logger.setLevel(settings.get('log_level'))
+
+    stdout_handler = StreamHandler(stdout)
+    stdout_handler.setFormatter(LoggerFormatter())
+
+    logger.addHandler(stdout_handler)
+
+    if 'prod' in getenv('ENV_FOR_DYNACONF'):
+        sentry_sdk.init(settings.get('sentry_dsn'))
+
+
+class LoggerFormatter(Formatter):
+
+    def format(self, record):
+        log = {
+            'host': gethostname(),
+            'level': self._get_log_level(record.levelname),
+            'message': record.msg,
+            'timestamp': record.created,
+            'time_human': str(datetime.now()),
+            '_application': 'drink-delivery-api',
+            '_environment': getenv('ENV_FOR_DYNACONF'),
+            '_log_type': 'application'
+        }
+
+        if 'ERROR' in record.levelname and record.exc_info:
+            log['_traceback'] = format_exc()
+
+        return dumps(log)
+
+    def _get_log_level(self, level):
+        return {
+            'CRITICAL': 2,
+            'DEBUG': 7,
+            'ERROR': 3,
+            'INFO': 6,
+            'WARNING': 4,
+        }.get(level, 6)
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from app.api import app as application
 
 
 if __name__ == '__main__':
-    run('main:application', host='0.0.0.0', port=8000, workers=4)
+    run('main:application', host='0.0.0.0', port=5000, workers=4)
