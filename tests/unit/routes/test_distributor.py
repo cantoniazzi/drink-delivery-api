0538e5955338983c39dcf79a9c9537e39b7ce968
cantoniazzi <cassiosvaldo@gmail.com>
2021-03-23 15:17:41 -0300

    adds find by lat long unit tests

--- tests/unit/routes/test_distributor.py
+++ tests/unit/routes/test_distributor.py
@@ -5,10 +5,11 @@ import pytest
 
 from app.routes.distributor import create_distributor
 from app.routes.distributor import get_distributor_by_id
+from app.routes.distributor import get_distributor_by_lat_long
 from app.schemas.distributor import DistributorBaseSchema
 
 
-def test_get_distributor_error_404(get_distributor):
+def test_get_distributor_by_id_error_404(get_distributor):
     with patch('app.routes.distributor.get_db') as mock_db_session:
         id = 1234
         mock_db_session.query.return_value.filter.return_value = None
@@ -17,13 +18,36 @@ def test_get_distributor_error_404(get_distributor):
             get_distributor_by_id(id, mock_db_session)
 
 
-def test_get_distributor_error_500(get_distributor):
+def test_get_distributor_by_id_error_500(get_distributor):
     with patch('app.routes.distributor.get_db') as mock_db_session:
         id = 1234
         mock_db_session.query.side_effect = Exception()
 
         with pytest.raises(HTTPException):
             get_distributor_by_id(id, mock_db_session)
+
+
+def test_get_distributor_lat_long_error_404(get_distributor):
+    with patch('app.routes.distributor.get_db') as mock_db_session:
+        lat, long = 10, 10
+        (
+            mock_db_session.query.return_value
+            .filter.return_value
+            .order_by.return_value
+            .first.return_value
+         ) = None
+
+        with pytest.raises(HTTPException):
+            get_distributor_by_lat_long(lat, long, mock_db_session)
+
+
+def test_get_distributor_by_lat_long_error_500(get_distributor):
+    with patch('app.routes.distributor.get_db') as mock_db_session:
+        lat, long = 10, 10
+        mock_db_session.query.side_effect = Exception()
+
+        with pytest.raises(HTTPException):
+            get_distributor_by_lat_long(lat, long, mock_db_session)
 
 
 def test_post_distributor_error(get_distributor):
